9b46510aef044fb62f5afe34480e4f740436d99d
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-10-18 12:31:59 +0600

    user@ICT-D19305 MINGW64 /e/Leetcode (main) $ git add .
    user@ICT-D19305 MINGW64 /e/Leetcode (main)
    $ git commit -m

---
+++ 367. Valid Perfect Square.cpp
@@ -0,0 +1,10 @@
+//367. Valid Perfect Square
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int x = sqrt(num);
+        if(x*x == num)return true;
+        return false;
+    }
+};
