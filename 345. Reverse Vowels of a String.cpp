b8d7048defe6451dc2f50a23865084f59831737f
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-10-23 08:41:00 +0600

    345. Reverse Vowels of a String

--- 345. Reverse Vowels of a String.cpp
+++ 345. Reverse Vowels of a String.cpp
@@ -1,32 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-string reverseVowels(string s) {
- 	int length = s.length();
- 	string ans = "";
- 	string vowel = "";
- 	for(int i = 0;i<length;i++){
- 		if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
- 			vowel+=s[i];
-		}
-	}
-	int ln = vowel.length()-1;
-	for(int i = 0;i<length;++i){
-		if(s[i] == 'a' || s[i] == 'e' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
- 			ans += vowel[ln];
- 			ln--;
-		}
-		else ans+=s[i];
-	}
-    return ans;
-}
-
-int main(){
-	string str;
-	cin >> str;
-	cout << reverseVowels(str);
-	
-	
-	
-	return 0;
-}
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int length = s.length();
+        char str[length];
+        for(int i = 0;i<length;i++)str[i] = s[i];
+        int st = 0;
+        int end = length-1;
+        while(st<end){
+            bool lv = false;
+            bool rv = false;
+            if(str[st] == 'a' || str[st] == 'e' || str[st] == 'i' || str[st] == 'o' || str[st] == 'u' || str[st] == 'A' || str[st] == 'E' 
+                || str[st] == 'I' || str[st] == 'O' || str[st] == 'U'){
+                lv = true;
+            }
+            if(str[end] == 'a' || str[end] == 'e' || str[end] == 'i' || str[end] == 'o' || str[end] == 'u' || str[end] == 'A' || str[end] == 'E' 
+                || str[end] == 'I' || str[end] == 'O' || str[end] == 'U'){
+                rv = true;
+            }
+            if(lv && rv){
+                char temp = str[st];
+                str[st] = str[end];
+                str[end] = temp;
+                st++;
+                end--;
+            }
+            else if(lv && !rv){
+                end--;
+            }
+            else st++;
+        }
+        string ans = "";
+        for(int i = 0;i<length;i++){
+            ans +=str[i];
+        }
+        return ans;
+    }
+};
