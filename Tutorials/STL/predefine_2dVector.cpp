6b585c3beb8419e805edcc68d5a07789dbbc0aa0
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-11-01 10:35:47 +0600

    2d matrix predefine elements changed update

--- Tutorials/STL/predefine_2dVector.cpp
+++ Tutorials/STL/predefine_2dVector.cpp
@@ -5,7 +5,11 @@ int main(){
 	
 	int row,col;
 	cin >> row >> col;
-	vector<vector<int> > matrix(row,vector<int>(col));
+	vector<vector<int> > matrix(row,vector<int>(col)); // the 2d matrix all elements will be 0 
+	
+	vector<vector<int> > xtrix(row,vector<int>(col,1)) // the 2d matrix all elements will be 1 
+	
+	
 	for(int i = 0;i<row;i++){
 		for(int j = 0;j<col;j++){
 			cout << matrix[i][j] << " ";
