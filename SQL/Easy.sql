3097529dee927e614e4791e33a167c649f88a848
ruhul <ra.01403288711@gmail.com>
2023-05-28 16:08:45 +0600

    175 combine two tables

--- SQL/Easy.sql
+++ SQL/Easy.sql
@@ -2,3 +2,13 @@
 --182 Dublicate emails:
 select email from Person 
 group by email having Count(*)>1
+
+
+
+--175 Combine Two Tables 
+select t1.firstName,t1.lastName,t2.city,t2.state
+from Person t1 
+LEFT JOIN
+Address t2
+ON
+t1.personId = t2.personId;
