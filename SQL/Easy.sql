c71af715c8a78591c71f4a03541ac7e81cce6802
ruhul <ra.01403288711@gmail.com>
2023-06-06 19:51:19 +0600

    1890. The Latest Login in 2020

--- SQL/Easy.sql
+++ SQL/Easy.sql
@@ -99,6 +99,14 @@ HAVING SUM(t2.unit)>=100
 
 
 
+--1890. The Latest Login in 2020
+SELECT user_id,MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE time_stamp LIKE  "2020-%"
+GROUP BY user_id
+
+
+
 --1075. Project Employees I
 SELECT t1.project_id,ROUND(avg(t2.experience_years),2) as average_years FROM 
 Project t1
