9ea6ce21d728e16cff7069c1cf17115d5dc0a342
ruhul <ra.01403288711@gmail.com>
2023-06-06 19:08:47 +0600

    1068. Product Sales Analysis I

--- SQL/Easy.sql
+++ SQL/Easy.sql
@@ -124,3 +124,12 @@ AND
 description !="boring"
 ORDER BY
 rating desc
+
+
+
+-- 1068. Product Sales Analysis I
+SELECT t1.product_name,t2.year,t2.price
+FROM Product t1
+JOIN
+Sales t2 
+ON t1.product_id = t2.product_id
