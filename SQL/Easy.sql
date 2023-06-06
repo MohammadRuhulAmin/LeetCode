9f2aba37a86954d987fc2b1fb6b85fb2cca4b4b0
ruhul <ra.01403288711@gmail.com>
2023-06-06 19:00:29 +0600

    620. Not Boring Movies

--- SQL/Easy.sql
+++ SQL/Easy.sql
@@ -113,3 +113,14 @@ t1.project_id
 UPDATE Salary
 SET
 Sex = if(sex = 'm','f','m')
+
+
+--620. Not Boring Movies
+SELECT id,movie,description,rating
+FROM
+Cinema
+WHERE id%2 != 0
+AND 
+description !="boring"
+ORDER BY
+rating desc
