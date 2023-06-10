96845b8b9f1634d1c67e8d2ca68f70ec0e15b570
ruhul <ra.01403288711@gmail.com>
2023-06-10 17:45:06 +0600

    1873. Calculate Special Bonus

--- SQL/Easy.sql
+++ SQL/Easy.sql
@@ -271,3 +271,12 @@ WHERE employee_id NOT IN
 (SELECT employee_id FROM Employees)
 ORDER BY employee_id ASC
  
+
+
+
+-- 1873. Calculate Special Bonus
+ SELECT 
+    employee_id,
+    IF(employee_id%2 !=0 AND name NOT LIKE('M%'),salary,0) AS bonus
+FROM  Employees
+ORDER BY employee_id
