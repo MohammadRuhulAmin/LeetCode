0cb2336b49cba16cde7144431d8ab8e721cb85ab
ruhul <ra.01403288711@gmail.com>
2023-06-11 17:10:58 +0600

    1251. Average Selling Price

--- SQL/LeetCode_SQL/Easy.sql
+++ SQL/LeetCode_SQL/Easy.sql
@@ -344,6 +344,18 @@ GROUP BY teacher_id
 
 
 
+-- 1251. Average Selling Price
+SELECT P.product_id, 
+    ROUND(SUM(P.price*U.units)/SUM(U.units),2) AS average_price
+FROM Prices P
+LEFT JOIN UnitsSold U
+ON P.product_id = U.product_id
+WHERE U.purchase_date BETWEEN P.start_date AND P.end_date
+GROUP BY product_id
+
+
+
+
 
 
 
