fa1486c39c9638a4a68162c9fb997faed731c983
ruhul <ra.01403288711@gmail.com>
2023-06-11 08:47:54 +0600

    OFFSET in MYSQL

---
+++ SQL/Study/offset.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Table_name LIMIT OFFSET,RANGE;
+
+OFFSET means Start FROM the Row of a table .. 
+
+
+Example : 
+SELECT * FROM Employees LIMIT 4,100;
+
+Means the select statement will select the row from 4th not 1st . Then 
+fetch 4th to 104th rows means 104-4 = 100 rows From Employee table  
