dddb310b1a631acf33fe16895c7fe6c871593882
ruhul <ra.01403288711@gmail.com>
2023-06-15 20:12:16 +0600

    rank,dense_rank,row_number,over clause

---
+++ SQL/Study/rank_over.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees(
+	id int AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30),
+    dep_id int,
+    salary int
+);
+
+
+INSERT INTO employees(name,dep_id,salary)VALUES("ruhul",1,300);
+INSERT INTO employees(name,dep_id,salary)VALUES("sakib",1,20000);
+INSERT INTO employees(name,dep_id,salary)VALUES("sajid",2,15000);
+INSERT INTO employees(name,dep_id,salary)VALUES("sai",2,1200);
+INSERT INTO employees(name,dep_id,salary)VALUES("pothik",3,1000);
+INSERT INTO employees(name,dep_id,salary)VALUES("kurban",3,4500);
+INSERT INTO employees(name,dep_id,salary)VALUES("panai",4,30000);
+INSERT INTO employees(name,dep_id,salary)VALUES("ponkoj",4,30000);
+INSERT INTO employees(name,dep_id,salary)VALUES("john",4,30);
+INSERT INTO employees(name,dep_id,salary)VALUES("Joshim",3,10);
+
+
+SELECT id ,name , dep_id,salary ,
+RANK() OVER(ORDER BY salary DESC,dep_id ASC) AS rnk ,
+DENSE_RANK() OVER(ORDER BY salary DESC) AS drnk,
+ROW_NUMBER() OVER(ORDER BY salary DESC ) AS rw
+FROM employees;
