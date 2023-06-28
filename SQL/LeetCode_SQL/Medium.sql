3db2e0a76f5e0b9fcfddb153e67b006db0942a70
ruhul <ra.01403288711@gmail.com>
2023-06-28 21:13:51 +0600

    608. Tree Node

--- SQL/LeetCode_SQL/Medium.sql
+++ SQL/LeetCode_SQL/Medium.sql
@@ -241,8 +241,16 @@ FROM CTE2,(SELECT COUNT(DISTINCT customer_id) AS cid FROM  Delivery) AS countQue
 
 -- ##############################################
 
+-- 608. Tree Node
 
 
-
+SELECT id,
+CASE 
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id NOT IN(SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf'
+    ELSE 'Inner'
+END
+AS 'type'
+FROM Tree
 
 
