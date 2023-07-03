9195002cb9da1485764feb44d51ab93ddcb19546
ruhul <ra.01403288711@gmail.com>
2023-07-03 23:46:59 +0600

    added lead and lag

--- SQL/Study/WindowFunction.sql
+++ SQL/Study/WindowFunction.sql
@@ -46,6 +46,39 @@ ROW_NUMBER():
     ) AS tbl 
     WHERE tbl.RowNo <3;
 
- 
+ ** PARTITION is used as GROUP BY in window function ~~~
+
+LEAD LAG :
+
+    LEAD(expression, offset, default) OVER (PARTITION BY ... ORDER BY ...)
+
+    SELECT E.*,
+    LAG(salary,2,0) OVER(PARTITION BY dept_name ORDER BY emp_id) AS 'prev',
+    LEAD(salary,2,0) OVER(PARTITION BY dept_name ORDER BY emp_id) AS 'next'
+    FROM Employee E; 
+
+Let's break down each part of the syntax:
+
+    expression: This is the column or expression from which you want to retrieve the subsequent value.
+    offset: This specifies the number of rows ahead from the current row where you want to retrieve the value. In your case, the offset is 2, meaning you want to retrieve the value of the column two rows ahead.
+    default: This is an optional parameter that specifies the value to return if there is no subsequent row available. If you don't provide a default value, it will return NULL by default.
+    PARTITION BY: This is an optional clause that allows you to divide the rows into partitions based on one or more columns. If you omit this clause, the LEAD() function will consider the entire result set as a single partition.
+    ORDER BY: This specifies the ordering of the rows within each partition. You can specify one or more columns to define the order.
+
+Here's an example that demonstrates the usage of LEAD() function to retrieve the salary of the next row, two rows ahead, from a table called employees:
+
+SELECT salary, LEAD(salary, 2, 0) OVER (ORDER BY employee_id) AS next_salary
+FROM employees;
+
+In this example, the salary column represents the current row's salary, and the LEAD(salary, 2, 0) expression retrieves the salary of the row that is two positions ahead. The OVER (ORDER BY employee_id) clause specifies that the ordering should be based on the employee_id column.
+
+The result of the query will include the salary column and a new column called next_salary, which contains the salary of the row two positions ahead. If there is no subsequent row available, the next_salary column will have the default value specified (0 in this case).
+
+Note: The LEAD() function is available in MySQL 8.0 and later versions. If you are using an older version of MySQL, the LEAD() function may not be available, and you would need to use alternative approaches like self-joins or subqueries to achieve similar results.
+
+
+
+
+
 
 TUTORIAL LINK : https://www.youtube.com/watch?v=Ww71knvhQ-s&t=11s
