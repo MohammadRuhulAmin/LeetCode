7d6239401528211f5c8a7dd88a23d1a1967e1d1f
ruhul <ra.01403288711@gmail.com>
2023-07-04 12:14:17 +0600

    how to study sql for getting better jobs

---
+++ SQL/Study/topics/syllabus.sql
@@ -0,0 +1,31 @@
+1. BASIC SQL Topics : 
+    1.1 Concepts : 
+        a. Relational Database 
+        b. Sql Commands (DDL,DML,DCL,TCL,DQL)
+        c. Normalization(1NF,2NF,3NF,BCNF)
+        d. Constraints (Primary Key , Foreign Key, Check Constraint, NOT NULL Constraint, Default)
+        e. Operators (UNION, UNION ALL, Logical,Comperation,Arithmatic) 
+        f. Important SQL Clause like(DISTINCT , Order By , Limit, Top Clause)
+        g. Practise (StrataScratch , LeedCode, DataLemur, datawithdanny)
+
+2. Intermideate SQL Topics :
+    2.1 Concepts : 
+        a. Group By And Having Clause 
+        b. Aggregate Functions 
+        c. Order of Execution 
+        d. Sub-queries 
+        e. CTE table / With Clause 
+        f. All Join 
+        g. Self join 
+        h. in-built functions 
+        i. Window functions (rank,dense_rank,lead,lag) (First_value,last_value,nth_value)
+        j.Views 
+3. Advanced SQL Topics :
+    3.1 Concepts :
+        a. Recursive SQL Query 
+        b. PIVOT Table / CrossTab function 
+        c. Materialized Views 
+        d. Stored Procedure 
+        e. User Defined Functions 
+        
+
