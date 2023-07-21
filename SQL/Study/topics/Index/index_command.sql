f389d4abddf518482294bba9913002385d40dd74
ruhul <ra.01403288711@gmail.com>
2023-07-21 21:09:11 +0600

    basic indexing on a table

---
+++ SQL/Study/topics/Index/index_command.sql
@@ -0,0 +1,22 @@
+
+Index command : 
+    CREATE INDEX index_name 
+    ON table_name(column1,column2,column3,....);
+
+Drop command :
+ DROP INDEX index_name
+ ON table_name;
+
+SHOW INDEX command:
+    SHOW INDEX FROM students;
+    
+
+
+WHY we use indexin ? 
+==> INDEX columns that are used for joins to imporve join performance.
+
+Important Point : 
+1. Automatically creates the indexes for PRIMARY KEY AND UNIQUE columns 
+2. index columns that you frequently use to retrieve the data. 
+3. Avoid columns that contain too many NULL values.
+4.Small tables do not require indexes.
