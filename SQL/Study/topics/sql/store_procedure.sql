2ee923f77ce8f69cb3334fa471d2c12348aad352
ruhul <ra.01403288711@gmail.com>
2023-08-02 16:35:19 +0600

    added store procedure with examole

--- SQL/Study/topics/sql/store_procedure.sql
+++ SQL/Study/topics/sql/store_procedure.sql
@@ -11,3 +11,15 @@ CALL find_app_idv(166);
 
 
 DROP PROCEDURE find_app_idv;
+
+
+Example :
+DELIMITER $$
+    CREATE PROCEDURE find_customer(IN f_name VARCHAR(50),IN l_name VARCHAR(50))
+    BEGIN 
+        SELECT * FROM customers WHERE first_name = f_name 
+        AND last_name = l_name; 
+    END $$
+DELIMITER;
+
+CALL find_customer("RUHUL","AMIN");
