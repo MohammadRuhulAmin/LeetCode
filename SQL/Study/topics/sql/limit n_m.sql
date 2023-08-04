f17de4714fd9d0b3901a633e26af0aa7bccb0179
ruhul <ra.01403288711@gmail.com>
2023-08-05 00:41:20 +0600

    more on limit and offset

--- SQL/Study/topics/sql/limit n_m.sql
+++ SQL/Study/topics/sql/limit n_m.sql
@@ -15,6 +15,11 @@ With this query, you will indeed get the 3rd minimum date from the table.
 here limit 3 means skiping the first 3 row then print the row!
 
 select distinct created from date_sort order by created limit 3, 2;
-similarly here the 3 means the first 3 rows will be skiped and 2 means the next 2 rows will be selected and print 
+similarly here the 3 means the first 3 rows will be skiped and 2 means the next 2 rows 
+will be selected and print 
 
+in a final :
+select * from table_name limit n,m;
+here m = limit value 
+and n = offset value 
 
